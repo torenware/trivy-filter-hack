95b4bdbb936c2210aa3688c1b74747f1fe809b6e
Rob Thorne <rob@torenware.com>
2024-08-20 08:57:40 -0700

    added some "edit me" comments in files

--- rewrite.sh
+++ rewrite.sh
@@ -1,9 +1,19 @@
 #! /usr/bin/env bash
 
+#
+# This script is a "front end" to the trivy binary. You'll
+# typically install it as "trivy" in some directory that will
+# be called before where the trivy binary is in the PATH.
+#
+
+# Edit this file to point to where you
+# installed the trivy-wrapper.py script
+WRAPPER_INSTALL_DIR=.
+
 if [ "$1" != "image" ]; then
   exec trivy $@
 else
-  if CMD=$(python3 trivy-wrap.py $@); then
+  if CMD=$(python3 $WRAPPER_INSTALL_DIR/trivy-wrap.py $@); then
     echo "command: $CMD"
     exec $CMD
   else
--- settings.py
+++ settings.py
@@ -1,1 +1,3 @@
+# edit this file to point to the actual
+# (aka "real") trivy binary:
 REAL_TRIVY = "/usr/local/bin/trivy"
